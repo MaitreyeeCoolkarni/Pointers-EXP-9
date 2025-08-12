c97e144ce7a426eb8976e668ae305304115a6dbb
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-08-12 13:58:34 +0530

    Create String input.cpp

---
+++ String input.cpp
@@ -0,0 +1,17 @@
+//Program-4
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string str1="";
+
+    cout<<"Enter the String: ";
+    cin>>str1;
+
+    string* str_ptr=&str1;
+
+    cout<<"Entered String is: "<<*str_ptr;
+
+    return 0;
+}
